3cd65d8499ec664d8eddf20972aefa445a3d34e4
toxicdragoneye <36698043+toxicdragoneye@users.noreply.github.com>
2018-12-23 16:19:34 +0100

    werkende versie + erlang buizenstelsel

--- src/hello_erlang2_app.erl
+++ src/hello_erlang2_app.erl
@@ -4,31 +4,15 @@
 -export([start/2]).
 -export([stop/1]).
 
-% clear TCP listener + secure TLS listener in comment
-% Clients connecting to Cowboy on the clear listener port are expected to use either HTTP/1.1 or HTTP/2
-
-% API
 
 start(_Type, _Args) ->
-	 %Dispatch = cowboy_router:compile([
-     %   {"/", cowboy_static, {priv_file, hello_erlang2_app, "index.html"}}
-    %])
-    Dispatch = cowboy_router:compile([ {'_', [{"/", hello_handler, []}]}]),
+	Dispatch = cowboy_router:compile([ {'_', [{"/", cowboy_static, {priv_file, hello_erlang2, "index.html"}}]}]),
+    %Dispatch = cowboy_router:compile([ {'_', [{"/", hello_handler, []}]}]),
     {ok, _} = cowboy:start_clear(my_http_listener,
        [{port, 8080}],
        #{env => #{dispatch => Dispatch}}
     ),
-    %{ok, _} = cowboy:start_tls(my_http_listener,
-    %    [
-    %        {port, 8443},
-    %        {certfile, "/path/to/certfile"},
-    %        {keyfile, "/path/to/keyfile"}
-    %    ],
-    %    #{env => #{dispatch => Dispatch}}),
     hello_erlang2_sup:start_link().
 
 stop(_State) ->
 	ok.
-
-
-%% internal functions
