29645c60a9865750ae477961795ae0596e7caa3e
toxicdragoneye <36698043+toxicdragoneye@users.noreply.github.com>
2018-12-27 16:24:09 +0100

    debugged directories + inserted dynamic image handler

--- src/hello_erlang2_app.erl
+++ src/hello_erlang2_app.erl
@@ -7,8 +7,20 @@
 
 start(_Type, _Args) ->
 	Dispatch = cowboy_router:compile([ {'_', [
-                                                {"/", cowboy_static, {priv_file, hello_erlang2, "main.html"}},
-                                                {"/square_installations/[...]", cowboy_static, {priv_file, hello_erlang2, "square_installations/pipes.html"}}
+                                                {"/", cowboy_static, {priv_file, hello_erlang2, "main.html", [{mimetypes, cow_mimetypes, all}]}},
+                                                %{"/index", cowboy_static, {priv_file, hello_erlang2, "index.html"}},
+                                                %pipes
+                                                {"/square_installations/pipes", cowboy_static, {priv_file, hello_erlang2, "square_installations/pipes.html", [{mimetypes, cow_mimetypes, all}]}},
+                                                {"/square_installations/pipes/image", image_handler, []},
+
+                                                %pipes_pump
+                                                {"/square_installations/pipes_pump", cowboy_static, {priv_file, hello_erlang2, "square_installations/pipes_pump.html", [{mimetypes, cow_mimetypes, all}]}},
+
+                                                %pipes_pump_flowmeter
+                                                {"/square_installations/pipes_pump_flowmeter", cowboy_static, {priv_file, hello_erlang2, "square_installations/pipes_pump_flowmeter.html", [{mimetypes, cow_mimetypes, all}]}},
+
+                                                %pipes_pump_flowmeter_heatexchanger
+                                                {"/square_installations/pipes_pump_flowmeter_heatexchanger", cowboy_static, {priv_file, hello_erlang2, "square_installations/pipes_pump_flowmeter_heatexchanger.html", [{mimetypes, cow_mimetypes, all}]}}
                                         ]}
                                     ]),
     
